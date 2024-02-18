a5e1ebf69489429e55c5a187a9955fe615eabb60
Isaac Figueiredo <isaac.lima.105@ufrn.edu.br>
2024-02-18 08:36:46 -0300

    up

--- js/main.js
+++ js/main.js
@@ -9,8 +9,14 @@ for (let card of cards) {
 let ejects = document.querySelectorAll('.eject');
 
 for (let eject of ejects) {
-	eject.addEventListener('click', (e) => {
-		e.preventDefault();
+	eject.addEventListener('click', () => {
 		window.open('https://www.ejectufrn.com.br/', '_blank');
 	});
 }
+
+
+let heroProg = document.querySelector('.hero-prog');
+heroProg.addEventListener('click', () => {
+	let section = document.querySelector('#programacao');
+	section.scrollIntoView({ behavior: 'smooth' });
+});
