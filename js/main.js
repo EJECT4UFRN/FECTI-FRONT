288766af388d652e9ca615b724aab4f97cd71ce8
Isaac Figueiredo <isaac.lima.105@ufrn.edu.br>
2024-02-20 21:38:30 -0300

    up

--- js/main.js
+++ js/main.js
@@ -20,3 +20,22 @@ heroProg.addEventListener('click', () => {
 	let section = document.querySelector('#programacao');
 	section.scrollIntoView({ behavior: 'smooth' });
 });
+
+
+function truncateString(str, num) {
+	if (str.length <= num) {
+		return str;
+	}
+	return str.substring(0, num) + '...';
+}
+
+let member_roles = document.querySelectorAll('.member-role');
+let member_name = document.querySelectorAll('.member-name');
+
+for (let role of member_roles) {
+	role.textContent = truncateString(role.textContent, 30);
+}
+
+for (let name of member_name) {
+	name.textContent = truncateString(name.textContent, 30);
+}
